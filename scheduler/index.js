6b56444cfc855dd3125d65599b75b594482de025
Howard <edt.chen@mail.utoronto.ca>
2020-09-27 15:11:12 -0700

    Testing out describe event bus

--- scheduler/index.js
+++ scheduler/index.js
@@ -1,8 +1,23 @@
 const { Assignment } = require("./assignment.js");
 const parse = require('csv-parse/lib/sync');
+const AWS = require("AWS");
+
+const EVENT_TAGS = {
+    Product: "UofTAssignments",
+};
+
+const eventbridge = new AWS.EventBridge();
 
 const clearReminders = () => {
     // clear all triggers from lambda UofTAssignmentRemind
+    const params = {
+        Name: "arn:aws:events:us-west-2:292002577318:event-bus/UofTAssignmentNotificationBus",
+    };
+
+    eventbridge.describeEventBus(params, (err, data) => {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+    });
 }
 
 const handleAssignment = (assignment) => {
